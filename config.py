30ba8ef3f2554c491e61ff03bf865b0be4085f53
Vater-v <v333iv@gmail.com>
2025-08-05 01:45:48 +0700

    1 test

---
+++ config.py
@@ -0,0 +1,7 @@
+class AppConfig:
+    APP_NAME = "OiAuto"
+    CURRENT_VERSION = "0.1"
+    GITHUB_REPO = "Vater-v/OiAuto"  # например, "VaterGit/oi-auto"
+    ASSET_NAME = "OiAuto.zip"  # имя архива в релизе (обновлять вручную)
+    MSG_UPDATE_FAIL = "Ошибка обновления."
+    LOBBY_MSG_UPDATE_CHECK = "Проверка обновлений..."
